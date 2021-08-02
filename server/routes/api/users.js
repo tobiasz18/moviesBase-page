cc09873ad32e3f0d11d30475cae722d8b2298501
tobiasz18 <tobiaszpost@gmail.com>
2021-08-02 18:58:10 +0200

    create patch endpoint

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -54,19 +54,39 @@ router.route('/signin')
   });
 // Test accescontrol npm,
 router.route("/profile")
-  .get(checkLoggedIn, grantAccess('readOwn','profile'), async (req, res) => {
+  .get(checkLoggedIn, grantAccess('readOwn', 'profile'), async (req, res) => {
     try {
       const permission = res.locals.permission;
 
-      const user = await User.findById(req.user._id);  
+      const user = await User.findById(req.user._id);
       if (!user) return res.status(400).json({ message: "bad user" });
 
       res.status(200).json(permission.filter(user._doc));
-      
+
     } catch (error) {
       return res.status(400).send(error);
-    }   
-  });
+    }
+  })
+  .patch(checkLoggedIn, grantAccess('updateOwn', 'profile'), async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          "$set": {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            age: req.body.age
+          }
+        },
+        { new: true }
+      );
+      if(!user) return res.json({message: 'User not found'});
+
+      res.status(200).json(getUserProps(user));
+    } catch (error) {
+      res.status(400).json({message: 'Problem updating', error: error});
+    }
+  })
 
 const getUserProps = (user) => {
   return {
