dd3ea9d4328517c8c73ad88ad1b0ff297bcc27f1
tobiasz18 <tobiaszpost@gmail.com>
2022-03-26 23:33:29 +0100

    Admin dashboard / categories ui

--- client/src/components/dashboard/categories/add.js
+++ client/src/components/dashboard/categories/add.js
@@ -1,12 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react';
+import LayoutDashboard from '../../../hoc/LayoutDashboard';
+import { useDispatch } from 'react-redux'
 
-const AddCategory = () => {
+import { useFormik } from 'formik'
+
+import * as Yup from 'yup'
+
+import { TextField, Button } from '@mui/material'
+import { FormGroup } from '@mui/material';
+
+
+
+const Add = () => {
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: { name: '' },
+    validationSchema: Yup.object({
+      name: Yup.string().required("The name is required")
+    }),
+    onSubmit: (values, { resetForm }) => {
+      resetForm()
+    }
+  })
+
+  const errorHelper = (formik, name) => ({
+    error: formik.errors[name] && formik.touched[name] ? true : false,
+    helperText: formik.errors[name] && formik.touched[name] ? formik.errors[name] : null
+  })
+
 
   return (
-    <div>HI AddCategory</div>
+
+    <form onSubmit={formik.handleSubmit}>
+      <FormGroup sx={{marginBottom: '20px'}}>
+        <TextField
+          style={{ width: '100%' }}
+          name="name"
+          label="Enter a name"
+          variant='outlined'
+          {...formik.getFieldProps('name')}
+          {...errorHelper(formik, 'name')}
+        />
+      </FormGroup>
+
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+      >
+        Add category
+      </Button>
+    </form>
+
   );
 }
 
-export default AddCategory;
-
+export default Add;
--- client/src/components/dashboard/categories/index.js
+++ client/src/components/dashboard/categories/index.js
@@ -1,13 +1,83 @@
 import React, { useState, useEffect } from 'react';
 import LayoutDashboard from '../../../hoc/LayoutDashboard';
+import { useDispatch, useSelector } from 'react-redux'
+import { styled } from '@mui/system';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
+import Add from './add';
+
+import Grid from '@mui/material/Grid';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+const Root = styled('div')`
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  td,
+  th {
+    border: 1px solid #ddd;
+    text-align: left;
+    padding: 8px;
+  }
+
+  th {
+    background-color: #0062cc;
+    color: #fff;
+  }
+`;
+
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    
+  }
+}))
 
 const Categories = () => {
-  return (  
-    <LayoutDashboard section="Categories"> 
-        <div>Welcom in categories</div>
-    </LayoutDashboard>
+  const classes = useStyles();
+  const matches = useMediaQuery('(max-width:600px)');
+
+
+  return (
+    <LayoutDashboard section="Categories">
+      <Grid container spacing={2}>
+
+        <Grid item xs={matches ? 12 : 4 }>
+          <Root>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Category Name</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow >
+                  <TableCell align="left">Action</TableCell>
+                </TableRow>
+                <TableRow >
+                  <TableCell align="right">Comedy</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Root>
+        </Grid>
+
+        <Grid item xs={matches ? 12 : 8}>
+          <Add />
+        </Grid>
+
+      </Grid>
+    </LayoutDashboard >
   );
 }
- 
+
 export default Categories;
